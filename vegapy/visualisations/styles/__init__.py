6deb90a5a3b508c094e9e6e9c64bd32d38861e05
Charlie <charlie@vegaprotocol.io>
2024-06-04 09:11:01 +0100

    feat: add vega themes

---
+++ vegapy/visualisations/styles/__init__.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def light():
+    plt.style.use(
+        [
+            "vegapy/visualisations/styles/theme-light.mplstyle",
+        ]
+    )
+
+
+def dark():
+    plt.style.use(
+        [
+            "vegapy/visualisations/styles/theme-dark.mplstyle",
+        ]
+    )
